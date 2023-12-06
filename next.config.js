a92bca5b0e41252203e6363f3c7043c2f94ea369
mohsinking2002 <mohsinraja4667@gmail.com>
2023-12-06 09:24:25 +0530

    added changes

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['localhost', 'api.goodnesslandboise.com']
-  },
-  distDir: 'build'
+  }
 };
 
 module.exports = nextConfig;
